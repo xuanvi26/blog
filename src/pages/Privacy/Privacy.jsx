bf0b32387081efce07a8d85345ac2fb3b6a5928e
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2018-10-08 16:39:30 -0400

    Add home page headlines and add text body to privacy page

--- src/pages/Privacy/Privacy.jsx
+++ src/pages/Privacy/Privacy.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 
 import {default as Banner} from '../../components/Banner/Banner';
 import {default as Footer} from '../../components/Footer/Footer';
+import '../../App.css'
 
 export default (props) => {
     const {pages, logo, creator, copyrightName, year} = props;
     return (
         <div>
             <Banner image="https://i.imgur.com/JlZlrLv.jpg" title='PRIVACY POLICY'/>
+            <div className='subtitle'>WE RESPECT YOUR PRIVACY.</div>
+            <p className='text'>
+                We are committed to protecting your privacy. For us, the protection 
+                not only of personal but of all data that flows through our platform is the 
+                top priority. As part of this effort, we process personal data in accordance 
+                with the EU’s General Data Protection Regulation (“GDPR”), and in accordance 
+                with the data protection regulations applicable to our website.
+            </p>
             <Footer pages={pages} logo={logo} creator={creator} copyrightName={copyrightName} year={year}/>
         </div>
     )
