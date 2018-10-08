a92ab7ca79b775311f162deb788abf6da1164634
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2018-10-07 21:59:58 -0400

    Create and style home banner

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom'
 
 import './App.css';
-import About from './components/About';
-import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header>
-          <Link to="/">Home </Link>
-          <Link to="/about-us">About </Link>
-        </header>
-        <main>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about-us" component={About} />
-        </main>
-      </div>
+          <div className="banner">
+            <img className="banner__img" alt="home-banner" src="https://i.imgur.com/JlZlrLv.jpg"/>
+            <div className="banner__title">KENDRA NGUYEN</div>
+          </div>
     );
   }
 }
