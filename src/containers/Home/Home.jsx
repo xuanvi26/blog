ccbe65d2336706fe861e7f9374446fc04f297a2c
Xuan-Vi Trinh-Nguyen <xuanvi.trinhnguyen@gmail.com>
2018-10-08 15:25:56 -0400

    Update Home container to pass slider props to page

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 import Home from '../../pages/Home/Home'
 
 import * as constants from '../../constants/constants';
+import tileData from './Sliders/tileData';
 
 export default () => {
     return (
         <div>
-            <Home pages={constants.PAGES} logo={constants.LOGO} creator={constants.CREATOR} copyrightName={constants.COPYRIGHTNAME} year={constants.YEAR} />
+            <Home sliders={tileData} pages={constants.PAGES} logo={constants.LOGO} creator={constants.CREATOR} copyrightName={constants.COPYRIGHTNAME} year={constants.YEAR} />
         </div>
     )
 }
