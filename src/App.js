8e4dadef5841939976d8bec2a6a89bacab69c316
Xuan-Vi Trinh-Nguyen <37204028+xuanvi26@users.noreply.github.com>
2018-10-08 12:12:39 -0400

    Add banner component (#7)
    * Fix spacing between slideshow titles and add fallback font
    
    * Implement banner component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import AutoSlideshow from './components/Slideshow/AutoSlideshow';
+import {default as Banner} from './components/Banner/Banner';
 
 class App extends Component {
   render() {
     return (
       <div>
-          <div className="banner">
-            <img className="banner__img" alt="home-banner" src="https://i.imgur.com/JlZlrLv.jpg"/>
-            <div className="banner__title">KENDRA NGUYEN</div>
-          </div>
-          <div>
-            <AutoSlideshow title={"ROOM"} subtitle={"MAKEOVER"} interval={7000} images={["https://i.imgur.com/gLQejaA.jpg", "https://i.imgur.com/UfYuYbO.jpg"]} rightIcon={"https://i.imgur.com/iqzjxvY.png"}/>
-          </div>
+          <Banner image="https://i.imgur.com/JlZlrLv.jpg" title='KENDRA NGUYEN'/>
+          <AutoSlideshow title={"ROOM"} subtitle={"MAKEOVER"} interval={7000} images={["https://i.imgur.com/gLQejaA.jpg", "https://i.imgur.com/UfYuYbO.jpg"]} rightIcon={"https://i.imgur.com/iqzjxvY.png"}/>
       </div>
     );
   }
